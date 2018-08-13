54c8e2bd7edaadc1443b27946e0b8db035e45e00
Noah Bratzel <noah@noahbratzel.com>
2018-08-13 07:41:40 -0500

    add gameboard and cell components

---
+++ src/components/Cell.vue
@@ -0,0 +1,35 @@
+<template>
+    <font-awesome-icon :icon="icon" @click="cellClick()" class="game-cell" />
+</template>
+
+<script>
+
+  export default {
+    name: "Cell",
+    data: function() {
+      return {
+        icon: "flag"
+      }
+    },
+    props: {
+      w: Number,
+      h: Number,
+    },
+    methods: {
+      cellClick() {
+        // console.log(this.icon, this.w, this.h);
+        this.icon = "dice-one";
+        this.$emit('cell-click', this.w, this.h);
+      }
+    },
+    components: {
+    },
+  }
+</script>
+
+<style scoped>
+    .game-cell {
+        width: 22px;
+        height: 22px;
+    }
+</style>
---
+++ src/components/GameBoard.vue
@@ -0,0 +1,44 @@
+<template>
+    <table>
+        <tr v-for="h in height" :key="h">
+            <td v-for="w in width" :key="w">
+                <Cell :id="'cell_' + w + '_' + h" v-bind:w="w" v-bind:h="h" v-on:cell-click="onCellClick"/>
+            </td>
+        </tr>
+    </table>
+</template>
+
+<script>
+  import Cell from './Cell.vue'
+
+  export default {
+    name: "GameBoard",
+    props: {
+      width: Number,
+      height: Number
+    },
+    components: {
+      Cell
+    },
+    methods: {
+      onCellClick: function (/*w, h*/) {
+        // console.log(w);
+        // console.log(h);
+      }
+    },
+  }
+</script>
+
+<style scoped>
+    table {
+        margin: auto;
+    }
+    td {
+        width: 30px;
+        height: 30px;
+        background-color: #ccc;
+        vertical-align: middle;
+        text-align: center;
+        cursor: pointer;
+    }
+</style>
