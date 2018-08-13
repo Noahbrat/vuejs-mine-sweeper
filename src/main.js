011c0f082bfecf969dd57c79ce78d6810ab8f0ec
Noah Bratzel <noah@noahbratzel.com>
2018-08-13 07:36:26 -0500

    added some font awesome icons

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBomb, faFlag, faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix }
+  from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-Vue.config.productionTip = false
+library.add(faBomb, faFlag, faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
