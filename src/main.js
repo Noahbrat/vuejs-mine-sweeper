785b8e4f354dc41363c3e2dacb52e40461a4964c
Noah Bratzel <noah@noahbratzel.com>
2018-08-25 23:21:05 -0500

    add icons for smile angry and grin-hearts

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBomb, faFlag, faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix }
+import { faBomb, faFlag, faSmile, faGrinHearts, faAngry,
+  faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix}
   from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faBomb, faFlag, faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix);
+library.add(faBomb, faFlag, faSmile, faGrinHearts, faAngry,
+  faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
